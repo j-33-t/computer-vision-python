71056062e0564746cbccc52f9071d38f618987dd
j-33-t <prasenjeet.rathore@gmail.com>
2022-04-10 11:40:04 +0200

    matplotlib-10/04/2022

--- 01_Basic_Image_Handling_&_Processing/1_2_Matplotlib.py
+++ 01_Basic_Image_Handling_&_Processing/1_2_Matplotlib.py
@@ -3,21 +3,29 @@ from PIL import Image
 from pylab import *
 import numpy as np
 
-# Read image to array
+# 1. Read image to array
 image = array(Image.open("./resources/original/messi-ronaldo.jpeg"))
 
-# Plot the blank canvas
+# 2. Plot the blank canvas
 imshow(image)
 
 # y axis starts from top left corner
-# adding some points to the chart
+# 3. adding some points to the chart
 x = np.array[100,100,400,400]
 y = np.array[200,500,200,500]
 
-# plot the points with read star-markers
-plot(x,y,'r*')
+# 4. PLOT
+ 
 
-# Line plot connecting the first two points
+plot(x,y,'r*') # points with red star-markers
+
+# plot(x,y,'go-') # green line with circle markers
+
+# plot(x,y,'ks:') # black dotted line with square-markers
+
+# 5. LINE PLOT 
+
+# connecting the first two points
 plot(x[0:2],y[:2])
 
 # More array slicing practicing
@@ -31,6 +39,6 @@ plot(x[0:4:2],y[1:3])
 plot(x[0:4:2],y[0:2])
 
 
-# Add title and show the plot
+# 6. Add title and show the plot
 title('Plotting: "Messi Ronaldo" ')
 show()

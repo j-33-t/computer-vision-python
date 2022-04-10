ffbaa9c5c009be9cce4b66e101da06458a8e976d
j-33-t <prasenjeet.rathore@gmail.com>
2022-04-10 11:34:03 +0200

    matplotlib-10/04/2022

--- 01_Basic_Image_Handling_&_Processing/1_2_Matplotlib.py
+++ 01_Basic_Image_Handling_&_Processing/1_2_Matplotlib.py
@@ -1,23 +1,35 @@
 # Imports
 from PIL import Image
 from pylab import *
+import numpy as np
 
 # Read image to array
-image = array(Image.open("./resources/exports/messi-ronaldo_GrayScale.png"))
+image = array(Image.open("./resources/original/messi-ronaldo.jpeg"))
 
-# Plot the image
+# Plot the blank canvas
 imshow(image)
 
 # y axis starts from top left corner
-# some points
-x = [100,100,400,400]
-y = [200,500,200,500]
+# adding some points to the chart
+x = np.array[100,100,400,400]
+y = np.array[200,500,200,500]
 
 # plot the points with read star-markers
 plot(x,y,'r*')
 
 # Line plot connecting the first two points
-plot(x[:2],y[:2])
+plot(x[0:2],y[:2])
+
+# More array slicing practicing
+# Straight Lines
+plot(x[0:4:2],y[0:4:2]) # x[first index:last index:interval steps]
+plot(x[2:4],y[0:2])
+plot(x[0:4:2],y[1:4:2])
+
+# Diagonal Lines
+plot(x[0:4:2],y[1:3])
+plot(x[0:4:2],y[0:2])
+
 
 # Add title and show the plot
 title('Plotting: "Messi Ronaldo" ')
